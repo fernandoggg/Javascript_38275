1ee2a08ed212654416f1815d9bbbc0369b62f60e
fernandoggg <fernando.guillermog@gmail.com>
2022-10-05 14:53:17 -0300

    correcciones de estilos, storage, entre otras

--- clase16/app.js
+++ clase16/app.js
@@ -2,7 +2,8 @@ let listaClientes = [];
 const objCliente = {
     id: '',
     nombre: '',
-    dni: ''
+    dni: '',
+    tel: ''
 }
 
 let editando = false;
@@ -10,14 +11,19 @@ let editando = false;
 const form = document.getElementById('form');
 const nombreInput = document.getElementById('nombre');
 const dniInput = document.getElementById('dni');
+const telInput = document.getElementById('tel');
 const btnAgregar = document.getElementById('btnAgregar');
 
 form.addEventListener('submit', validarFormulario);
 
 function validarFormulario(e){
    e.preventDefault();
-   if(nombreInput.value === '' || dniInput.value === '') {
-    alert("debe completar todos los campos");
+   if(nombreInput.value === '' || dniInput.value === '' || telInput.value === '') {
+     swal({
+         title: "Error debe completar todos los campos",
+         text: "Haga click para volver a completar",
+         icon: "error",
+       });
     return;
    } 
 
@@ -28,6 +34,10 @@ function validarFormulario(e){
     objCliente.id = Date.now();
     objCliente.nombre = nombreInput.value;
     objCliente.dni = dniInput.value;
+    objCliente.tel = telInput.value;
+    localStorage.setItem('Nombre y apellido:', objCliente.nombre)
+    localStorage.setItem('DNI:', objCliente.dni)
+    localStorage.setItem('Tel:', objCliente.tel)
 
     agregarCliente();
    }
@@ -42,26 +52,27 @@ function wipeBox(){
     objCliente.id = "";
     objCliente.nombre = "";
     objCliente.dni = "";
+    objCliente.tel = "";
 }   
 function mostrarClientes(){
     clearScreen();
     const divClientes = document.getElementById('div-clientes');
 
     listaClientes.forEach(cliente =>{
-        const {id, nombre, dni} = cliente;
+        const {id, nombre, dni, tel} = cliente;
         const parrafo = document.createElement('p');
-        parrafo.textContent = `${id} - ${nombre} - ${dni} - `;
+        parrafo.innerHTML = ` Name: ${nombre}, DNI: ${dni}, Tel: ${tel} . `;
         parrafo.dataset.id = id;
 
         const editarBoton = document.createElement('button');
         editarBoton.onclick = () => cargarCliente(cliente);
-        editarBoton.textContent = 'Modificar';
+        editarBoton.innerHTML = 'Modificar';
         editarBoton.classList.add('btn', 'btn-editar');
         parrafo.append(editarBoton);
 
         const borrarBoton = document.createElement('button');
         borrarBoton.onclick = () => borrarCliente(id);
-        borrarBoton.textContent = 'borrar';
+        borrarBoton.innerHTML = 'borrar';
         borrarBoton.classList.add('btn', 'btn-borrar');
         parrafo.append(borrarBoton);
 
@@ -73,30 +84,33 @@ function mostrarClientes(){
     });
 }
 function cargarCliente(cliente){
-    const {id, nombre, dni} = cliente;
+    const {id, nombre, dni, tel} = cliente;
     nombreInput.value = nombre;
     dniInput.value = dni;
+    telInput.value = tel;
 
     objCliente.id = id;
 
-    form.querySelector('button[type="submit"]').textContent = 'Actualizar';
+    form.querySelector('button[type="submit"]').innerHTML = 'Actualizar';
     editando = true;
 }
 function editarCliente(){
     objCliente.nombre = nombreInput.value;
     objCliente.dni = dniInput.value;
+    objCliente.tel = telInput.value;
 
     listaClientes.map( cliente => {
         if(cliente.id === objCliente.id){
             // cliente.id = objCliente.id;
             cliente.nombre = objCliente.nombre;
             cliente.dni = objCliente.dni;
+            cliente.tel = objCliente.tel;
         }
     });
     clearScreen();
     mostrarClientes();
     form.reset();
-    form.querySelector('button[type="submit"]').textContent = 'Agregar';
+    form.querySelector('button[type="submit"]').innerHTML = 'Agregar';
     editando = false;
 }
 
